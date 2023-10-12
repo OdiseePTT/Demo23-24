b7b33606970f088aeaa6d00a4aca5c42a673d1f0
Matthias <matthias.druwe@gmail.com>
2023-10-12 10:53:00 +0200

    Update

---
+++ Les_3/Les3/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace Les3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int> { 32, 12, 24, 56, 673, 24 };
+
+            //double sum = 0;
+
+            //foreach (int number in numbers)
+            //{
+            //    sum += number;
+            //}
+
+
+            //double average = sum / numbers.Count();
+
+            double average = numbers.Average();
+            double sum = numbers.Sum();
+            double min = numbers.Min();
+            double max = numbers.Max();
+
+
+            Console.WriteLine(average);
+            Console.WriteLine(sum);
+            Console.WriteLine(min);
+            Console.WriteLine(max);
+
+
+
+
+
+            List<Student> students = new List<Student>()
+            {
+                new Student("Mariam", "Legros", new DateTime(2001, 12, 1) ),
+                new Student("Kayley", "Thiel", new DateTime(1985, 1, 14) ),
+                new Student("Ewell", "Maggio", new DateTime(2003, 4, 30) ),
+                new Student("Evan", "Towne", new DateTime(2000, 2, 11) ),
+                new Student("Meggie", "Dach", new DateTime(2001, 2, 28) )
+            };
+
+
+
+        }
+    }
+}
---
+++ Les_3/Les3/Student.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Les3
+{
+    internal class Student
+    {
+        public Student(string firstName, string lastName, DateTime birthDate)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            BirthDate = birthDate;
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public int Age
+        {
+            get
+            {
+                int age = DateTime.Now.Year - BirthDate.Year;
+                if (BirthDate.Date > DateTime.Today.AddYears(-age))
+                {
+                    age--;
+                }
+                return age;
+            }
+        }
+    }
+}
