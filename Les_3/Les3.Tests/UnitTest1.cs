f561e946a32ea41ce6c61cac044903787798c8b0
Matthias <matthias.druwe@gmail.com>
2023-10-12 16:32:03 +0200

    Examples unit tests

--- Les_3/Les3.Tests/UnitTest1.cs
+++ Les_3/Les3.Tests/UnitTest1.cs
@@ -14,5 +14,91 @@ namespace Les3.Tests
 
             items.Should().BeEquivalentTo(reversedItems, (options) => options.WithStrictOrdering());
         }
+
+        [Fact]
+        public void DemoContains()
+        {
+            List<int> numbers = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+
+            Assert.Contains(numbers, (number) =>
+            {
+                return number == 1;
+            });
+
+        }
+
+        [Fact]
+        public void DemoAll()
+        {
+            List<int> numbers = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            Assert.All(numbers, (number) =>
+            {
+                Assert.NotEqual(10, number);
+            });
+
+            foreach (var number in numbers)
+            {
+                Assert.NotEqual(10, number);
+            }
+        }
+
+        [Fact]
+        public void DemoContain()
+        {
+            List<int> numbers = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            numbers.Should().Contain((number) => number%2 == 0);
+        }
+
+        [Fact]
+        public void DemoOnlyContain()
+        {
+            List<int> numbers = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            numbers.Should().OnlyContain((number) => number >= 0);
+        }
+
+
+        [Fact]
+        public void DemoException1()
+        {
+            int number1 = 2;
+            int number2 = 0;
+            Calculator sut = new Calculator();
+
+            // Act + Assert
+            Assert.Throws<DivideByZeroException>(()=> { double result = sut.Divide(number1, number2); });
+        }
+
+
+        [Fact]
+        public void DemoException2()
+        {
+
+            int number1 = 2;
+            int number2 = 0;
+            Calculator sut = new Calculator();
+
+            // Act + assert
+            sut.Invoking(sut => sut.Divide(number1,number2)).Should().Throw<DivideByZeroException>();
+        }
+
+        [Fact]
+        public void DemoException3()
+        {
+            // Arrange 
+            int number1 = 2;
+            int number2 = 0;
+            Calculator sut = new Calculator();
+
+            // Act
+            Action act = () => sut.Divide(number1, number2);
+
+            // Assert
+            act.Should().Throw<DivideByZeroException>();
+        }
+
     }
 }
---
+++ Les_3/Les3/Calculator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Les3
+{
+    public class Calculator
+    {
+
+        public double Divide(int number1, int number2)
+        {
+            return number1 / number2;
+        }
+    }
+}
